9b7a712aef18e317de088eb5e78b83c8779dcb99
lingxe01 <1014431405@qq.com>
2025-06-19 12:55:40 +0800

    Migrate to ROS 2 Humble and optimize CMake configuration

---
+++ src/pointpillars_test.cc
@@ -0,0 +1,109 @@
+// headers in STL
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+// headers in 3rd-part
+#include "../pointpillars/pointpillars.h"
+#include "gtest/gtest.h"
+using namespace std;
+
+/*
+ * 功能： 将测试点云txt文件修改为array，并存入到指针points_array
+ * 备注： txt点云测试文件，为n*5
+ */
+int Txt2Arrary( float* &points_array , string file_name , int num_feature = 4)
+{
+  ifstream InFile;
+  InFile.open(file_name.data());
+  assert(InFile.is_open());
+
+  vector<float> temp_points;
+  string c;
+
+  while (!InFile.eof())
+  {
+      InFile >> c;
+
+      temp_points.push_back(atof(c.c_str()));
+  }
+  points_array = new float[temp_points.size()];
+  for (int i = 0 ; i < temp_points.size() ; ++i) {
+    points_array[i] = temp_points[i];
+  }
+
+  InFile.close();  
+  return temp_points.size() / num_feature;
+  // printf("Done");
+};
+
+/*
+ * 功能： 将网络推理的boxes信息写到txt文件中，用于可视化脚本显示
+ */
+void Boxes2Txt( std::vector<float> boxes , string file_name , int num_feature = 7)
+{
+    ofstream ofFile;
+    ofFile.open(file_name , std::ios::out );  
+    if (ofFile.is_open()) {
+        for (int i = 0 ; i < boxes.size() / num_feature ; ++i) {
+            for (int j = 0 ; j < num_feature ; ++j) {
+                ofFile << boxes.at(i * num_feature + j) << " ";
+            }
+            ofFile << "\n";
+        }
+    }
+    ofFile.close();
+    return ;
+};
+
+
+// TEST(PointPillars, __build_model__) {
+int main(){
+  const std::string DB_CONF = "../bootstrap.yaml";
+  YAML::Node config = YAML::LoadFile(DB_CONF);
+
+  std::string pfe_file,backbone_file; 
+  if(config["UseOnnx"].as<bool>()) {
+    pfe_file = config["PfeOnnx"].as<std::string>();
+    backbone_file = config["BackboneOnnx"].as<std::string>();
+  }else {
+    pfe_file = config["PfeTrt"].as<std::string>();
+    backbone_file = config["BackboneTrt"].as<std::string>();
+  }
+  std::cout << backbone_file << std::endl;
+  const std::string pp_config = config["ModelConfig"].as<std::string>();
+  PointPillars pp(
+    config["ScoreThreshold"].as<float>(),
+    config["NmsOverlapThreshold"].as<float>(),
+    config["UseOnnx"].as<bool>(),
+    pfe_file,
+    backbone_file,
+    pp_config
+  );
+  std::string file_name = config["InputFile"].as<std::string>();
+  float* points_array;
+  int in_num_points;
+  in_num_points = Txt2Arrary(points_array,file_name,5);
+ 
+
+  
+  for (int _ = 0 ; _ < 10 ; _++)
+  {
+
+    std::vector<float> out_detections;
+    std::vector<int> out_labels;
+    std::vector<float> out_scores;
+
+    cudaDeviceSynchronize();//线程同步函数，等待gpu执行完成，再cpu执行
+    pp.DoInference(points_array, in_num_points, &out_detections, &out_labels , &out_scores);
+    cudaDeviceSynchronize();
+    int BoxFeature = 7;
+    int num_objects = out_detections.size() / BoxFeature;
+
+    std::string boxes_file_name = config["OutputFile"].as<std::string>();
+    Boxes2Txt(out_detections , boxes_file_name );
+    // EXPECT_EQ(num_objects,228);
+  }
+  return 0;
+
+};
